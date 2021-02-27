718cf84162de17efbf7f6b00d6ea9239772f2b47
kavishamodi97 <kavishamodi971@gmail.com>
2021-02-27 12:22:06 +0530

    Complete Image Hoster Endpoints

---
+++ technical-api/src/main/java/com/upgrad/technical/api/controller/AdminController.java
@@ -0,0 +1,61 @@
+package com.upgrad.technical.api.controller;
+
+
+import com.upgrad.technical.api.model.ImageDetailsResponse;
+import com.upgrad.technical.api.model.UpdateImageRequest;
+import com.upgrad.technical.api.model.UpdateImageResponse;
+import com.upgrad.technical.service.business.AdminService;
+import com.upgrad.technical.service.entity.ImageEntity;
+import com.upgrad.technical.service.exception.ImageNotFoundException;
+import com.upgrad.technical.service.exception.UnauthorizedException;
+import com.upgrad.technical.service.exception.UserNotSignedInException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/")
+public class AdminController {
+
+
+    @Autowired
+    private AdminService adminService;
+
+    @RequestMapping(method = RequestMethod.GET, path = "/images/{id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<ImageDetailsResponse> getImage(@PathVariable("id") final String imageUuid, @RequestHeader("authorization") final String authorization) throws ImageNotFoundException, UnauthorizedException, UserNotSignedInException {
+
+        final ImageEntity imageEntity = adminService.getImage(imageUuid, authorization);
+
+        ImageDetailsResponse imageDetailsResponse = new ImageDetailsResponse().image(imageEntity.getImage()).id((int) imageEntity.getId()).name(imageEntity.getName()).description(imageEntity.getDescription()).status(imageEntity.getStatus());
+
+
+        return new ResponseEntity<ImageDetailsResponse>(imageDetailsResponse, HttpStatus.OK);
+    }
+
+
+    @RequestMapping(method = RequestMethod.PUT, path = "/images/update/{image_id}", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<UpdateImageResponse> updateImage(final UpdateImageRequest updateImageRequest, @PathVariable("image_id") final long image_id, @RequestHeader("authorization") final String authorization) throws ImageNotFoundException, UnauthorizedException, UserNotSignedInException {
+        ImageEntity imageEntity = new ImageEntity();
+
+        //Notice that admin enters the id of the image to be updated which he gets in the Response Body when he gets the details of the image
+        //Set "Id", "Image", "Name", "Status" and "Description" of imageEntity using updateImageRequest and dynamic path variable "image_id" in the URI
+        //Write code here//
+        imageEntity.setImage(updateImageRequest.getImage());
+        imageEntity.setId(image_id);
+        imageEntity.setName(updateImageRequest.getName());
+        imageEntity.setStatus(updateImageRequest.getStatus());
+        imageEntity.setDescription(updateImageRequest.getDescription());
+
+        //Call the updateImage() method for adminService and pass imageEntity and authorization(access token) as an argument
+        //Using the ImageEntity type object returned by the updateImage() method set the attributes of UpdateImageResponse type object
+        //Return ResponseEntity<UpdateImageResponse> type object with Http status OK
+        //Write code here//
+        ImageEntity updatedimageEntity = adminService.updateImage(imageEntity, authorization);
+        UpdateImageResponse updateImageResponse = new UpdateImageResponse().id((int) updatedimageEntity.getId()).status(updatedimageEntity.getStatus());
+
+        return new ResponseEntity<UpdateImageResponse>(updateImageResponse, HttpStatus.OK);
+    }
+
+}
---
+++ technical-service/src/main/java/com/upgrad/technical/service/business/AdminService.java
@@ -0,0 +1,75 @@
+package com.upgrad.technical.service.business;
+
+
+import com.upgrad.technical.service.dao.ImageDao;
+import com.upgrad.technical.service.entity.ImageEntity;
+import com.upgrad.technical.service.entity.UserAuthTokenEntity;
+import com.upgrad.technical.service.exception.ImageNotFoundException;
+import com.upgrad.technical.service.exception.UnauthorizedException;
+import com.upgrad.technical.service.exception.UserNotSignedInException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class AdminService {
+
+    @Autowired
+    private ImageDao imageDao;
+
+    public ImageEntity getImage(final String imageUuid, final String authorization) throws ImageNotFoundException, UnauthorizedException, UserNotSignedInException {
+
+        UserAuthTokenEntity userAuthTokenEntity = imageDao.getUserAuthToken(authorization);
+
+        if (userAuthTokenEntity == null) {
+            throw new UserNotSignedInException("USR-001", "You are not Signed in, sign in first to get the details of the image");
+        }
+
+        String role = userAuthTokenEntity.getUser().getRole();
+        if (role.equals("admin")) {
+            ImageEntity imageEntity = imageDao.getImage(imageUuid);
+            if (imageEntity == null) {
+                throw new ImageNotFoundException("IMG-001", "Image with Uuid not found");
+            }
+            return imageEntity;
+        } else
+            throw new UnauthorizedException("ATH-001", "UNAUTHORIZED Access, Entered user is not an admin");
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public ImageEntity updateImage(final ImageEntity imageEntity, final String authorization) throws ImageNotFoundException, UnauthorizedException, UserNotSignedInException {
+        //Complete this method
+        //Firstly check whether the access token is a valid one(exists in user_auth_tokens table). If not valid throw UserNotSignedException
+        //Then check the role of the user with entered access token (if nonadmin then throw UnauthorizedException)
+        //If the role is admin, get the existing image in the database with entered image id using getImageById() method in ImageDao class
+        //If the image with entered image id does not exist throw ImageNotFoundException
+        //If the image with entered image id exists in the database and is returned, try to set all the attributes of the new image(received by this method) using the existing image
+        //Call updateImage() method for imageDao to update an image
+        //Note that ImageNotFoundException , UserNotFoundException and UnauthorizedException has been implemented
+        //Note that this method returns ImageEntity type object
+        UserAuthTokenEntity userAuthTokenEntity = imageDao.getUserAuthToken(authorization);
+
+        if (userAuthTokenEntity == null) {
+            throw new UserNotSignedInException("USR-001", "You are not Signed in, sign in first to update the details of the image");
+        }
+        String role = userAuthTokenEntity.getUser().getRole();
+        if (role.equals("admin")) {
+
+            ImageEntity existingimageEntity = imageDao.getImageById(imageEntity.getId());
+
+            if (existingimageEntity == null) {
+                throw new ImageNotFoundException("IMG-002", "Image with given id is not found");
+            }
+
+            imageEntity.setUuid(existingimageEntity.getUuid());
+            imageEntity.setNo_of_likes(existingimageEntity.getNo_of_likes());
+            imageEntity.setUser_id(existingimageEntity.getUser_id());
+            imageEntity.setCreated_at(existingimageEntity.getCreated_at());
+
+            return imageDao.updateImage(imageEntity);
+        } else {
+            throw new UnauthorizedException("ATH-001", "Unauthorized Access, Entered user is not an admin");
+        }
+    }
+}
